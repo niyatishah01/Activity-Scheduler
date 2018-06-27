f4c9787ef31174ce729f150df35558458348e4c8
Niyati Shah <niyatishah01@users.noreply.github.com>
2018-06-27 11:14:24 -0400

    Update Association.js

--- models/Association.js
+++ models/Association.js
@@ -1,6 +1,5 @@
 // Declaring the Attributes and functions related to association
 var mongoose = require('mongoose');
-const mongoose-fire = require('mongoose-fire');
 var Schema=  mongoose.Schema;
 
 var AssociationSchema = new Schema({
@@ -13,4 +12,3 @@ var AssociationSchema = new Schema({
 });
 
 module.exports = mongoose.model('Association', AssociationSchema);
-AssociationSchema.plugin(mongoose-fire);
